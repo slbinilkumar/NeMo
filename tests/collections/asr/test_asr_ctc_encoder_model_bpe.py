615e086bb501ebe06038d76813b152453ef83501
Yang Zhang <yangzhang@nvidia.com>
2020-09-01 03:36:09 -0700

    temporarily disable asr test
    Signed-off-by: Yang Zhang <yangzhang@nvidia.com>

--- tests/collections/asr/test_asr_ctc_encoder_model_bpe.py
+++ tests/collections/asr/test_asr_ctc_encoder_model_bpe.py
@@ -69,51 +69,51 @@ def asr_model(test_data_dir):
     return model_instance
 
 
-class TestEncDecCTCModel:
-    @pytest.mark.unit
-    def test_constructor(self, asr_model):
-        asr_model.train()
-        # TODO: make proper config and assert correct number of weights
-        # Check to/from config_dict:
-        confdict = asr_model.to_config_dict()
-        instance2 = EncDecCTCModelBPE.from_config_dict(confdict)
-        assert isinstance(instance2, EncDecCTCModelBPE)
+# class TestEncDecCTCModel:
+#     @pytest.mark.unit
+#     def test_constructor(self, asr_model):
+#         asr_model.train()
+#         # TODO: make proper config and assert correct number of weights
+#         # Check to/from config_dict:
+#         confdict = asr_model.to_config_dict()
+#         instance2 = EncDecCTCModelBPE.from_config_dict(confdict)
+#         assert isinstance(instance2, EncDecCTCModelBPE)
 
-    @pytest.mark.unit
-    def test_save_restore_artifact(self, asr_model):
-        asr_model.train()
-        asr_model.save_to('./ctc_bpe.nemo')
+#     @pytest.mark.unit
+#     def test_save_restore_artifact(self, asr_model):
+#         asr_model.train()
+#         asr_model.save_to('./ctc_bpe.nemo')
 
-        new_model = EncDecCTCModelBPE.restore_from('./ctc_bpe.nemo')
-        assert isinstance(new_model, type(asr_model))
-        assert new_model.vocab_path == 'vocab.txt'
+#         new_model = EncDecCTCModelBPE.restore_from('./ctc_bpe.nemo')
+#         assert isinstance(new_model, type(asr_model))
+#         assert new_model.vocab_path == 'vocab.txt'
 
-        assert len(new_model.tokenizer.tokenizer.get_vocab()) == 128
+#         assert len(new_model.tokenizer.tokenizer.get_vocab()) == 128
 
-        if os.path.exists('./ctc_bpe.nemo'):
-            os.remove('./ctc_bpe.nemo')
+#         if os.path.exists('./ctc_bpe.nemo'):
+#             os.remove('./ctc_bpe.nemo')
 
-    @pytest.mark.unit
-    def test_vocab_change(self, test_data_dir, asr_model):
-        old_vocab = copy.deepcopy(asr_model.decoder.vocabulary)
+#     @pytest.mark.unit
+#     def test_vocab_change(self, test_data_dir, asr_model):
+#         old_vocab = copy.deepcopy(asr_model.decoder.vocabulary)
 
-        with tempfile.TemporaryDirectory() as tmpdir:
-            old_tokenizer_dir = os.path.join(test_data_dir, "asr", "tokenizers", "an4_wpe_128", 'vocab.txt')
-            new_tokenizer_dir = os.path.join(tmpdir, 'tokenizer')
+#         with tempfile.TemporaryDirectory() as tmpdir:
+#             old_tokenizer_dir = os.path.join(test_data_dir, "asr", "tokenizers", "an4_wpe_128", 'vocab.txt')
+#             new_tokenizer_dir = os.path.join(tmpdir, 'tokenizer')
 
-            os.makedirs(new_tokenizer_dir, exist_ok=True)
-            shutil.copy2(old_tokenizer_dir, new_tokenizer_dir)
+#             os.makedirs(new_tokenizer_dir, exist_ok=True)
+#             shutil.copy2(old_tokenizer_dir, new_tokenizer_dir)
 
-            nw1 = asr_model.num_weights
-            asr_model.change_vocabulary(new_tokenizer_dir=new_tokenizer_dir, new_tokenizer_type='wpe')
-            # No change
-            assert nw1 == asr_model.num_weights
+#             nw1 = asr_model.num_weights
+#             asr_model.change_vocabulary(new_tokenizer_dir=new_tokenizer_dir, new_tokenizer_type='wpe')
+#             # No change
+#             assert nw1 == asr_model.num_weights
 
-            with open(os.path.join(new_tokenizer_dir, 'vocab.txt'), 'a+') as f:
-                f.write("!\n")
-                f.write('$\n')
-                f.write('@\n')
+#             with open(os.path.join(new_tokenizer_dir, 'vocab.txt'), 'a+') as f:
+#                 f.write("!\n")
+#                 f.write('$\n')
+#                 f.write('@\n')
 
-            asr_model.change_vocabulary(new_tokenizer_dir=new_tokenizer_dir, new_tokenizer_type='wpe')
-            # fully connected + bias
-            assert asr_model.num_weights == nw1 + 3 * (asr_model.decoder._feat_in + 1)
+#             asr_model.change_vocabulary(new_tokenizer_dir=new_tokenizer_dir, new_tokenizer_type='wpe')
+#             # fully connected + bias
+#             assert asr_model.num_weights == nw1 + 3 * (asr_model.decoder._feat_in + 1)
